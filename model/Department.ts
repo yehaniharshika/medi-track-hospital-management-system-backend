4db42e44d5ff183329e697cc07108b6505848080
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-02-08 15:24:47 +0530

    create Department.ts file

---
+++ model/Department.ts
@@ -0,0 +1,8 @@
+export default class Department{
+    departmentId!: string;
+    departmentName!: string;
+    departmentEmail!: string;
+    location!: string;
+    headOfDepartment!: string;
+    phoneNumber!: string;
+}
