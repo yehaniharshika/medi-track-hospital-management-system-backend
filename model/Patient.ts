0c2b129d45f765cc31b8ea94d4a7ea769cff8dac
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-02-12 17:09:25 +0530

    created Patient.ts

---
+++ model/Patient.ts
@@ -0,0 +1,14 @@
+export default class Patient{
+    patientId! : string;
+    patientName! : string;
+    age! : string;
+    patientImg! : string;
+    addressLine1!: string;
+    addressLine2!: string;
+    postalCode!: string;
+    gender!:string;
+    contactNumber!:string;
+    blood_type! : string;
+    chronic_diseases! :string;
+    last_visit_date! :string;
+}
