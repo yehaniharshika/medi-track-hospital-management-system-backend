e8a57414740e8edc1892c400fb45a6fe4c65092d
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-02-13 16:17:00 +0530

    created MedicalReport.ts

---
+++ model/MedicalReport.ts
@@ -0,0 +1,9 @@
+export default class MedicalReport{
+    medicalReportId!: string;
+    reportDate!: string;
+    testResults!: string;
+    notes!: string;
+    patientId!: string;
+    patientName!: string;
+    doctorId!: string;
+}
