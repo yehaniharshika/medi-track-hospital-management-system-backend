bb766d7752c7082467c6d8ff6ff9e8c51cc2ad7b
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-02-14 08:44:49 +0530

    created Appointment.ts file

---
+++ model/Appointment.ts
@@ -0,0 +1,9 @@
+export default class Appointment{
+    appointmentCode!: string;
+    appointmentDate!: string;
+    appointmentTime!: string;
+    patientId!: string;
+    doctorId!: string;
+    appointmentType!: string;
+    appointmentStatus!: string;
+}
