4d733ffccdf5067d23122351b0654633d64cb8e7
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-02-14 13:08:47 +0530

    successfully created Medicine.ts

---
+++ model/Medicine.ts
@@ -0,0 +1,10 @@
+export default class Medicine{
+    medicineId!: string;
+    medicineName!: string;
+    brand!: string;
+    medicineImg!: string;
+    dosage_form!: string;
+    unit_price!: string;
+    quantity_in_stock!: string;
+    expiry_date!: string;
+}
